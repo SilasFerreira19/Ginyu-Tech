886945f8f226ee6c0b747876e7a6c915974e4871
SilasFerreira21 <124620806+SilasFerreira19@users.noreply.github.com>
2025-06-28 10:44:27 -0300

    [feat] Update Dropdown

--- Bookadex-main/js/preloader.js
+++ Bookadex-main/js/preloader.js
@@ -19,16 +19,24 @@ document.addEventListener('DOMContentLoaded', function () {
 
 //=================== Dropdown ===============================================
 
-    function toggleDropdown() {
+    function toggleDropdown(event) {
+        event.stopPropagation(); // Impede que o clique se propague para o window
         const dropdown = document.getElementById("profileDropdown");
         dropdown.classList.toggle("show");
     }
+
+    document.getElementById("profileDropdown").addEventListener("click", function(event) {
+        event.stopPropagation(); // Evita que clique em links do dropdown feche ele
+    });
+
 
     // Fecha o dropdown ao clicar fora
     window.addEventListener('click', function(e) {
         const profile = document.querySelector('.imgprofile');
         const dropdown = document.getElementById("profileDropdown");
+
         if (!profile.contains(e.target)) {
             dropdown.classList.remove("show");
         }
     });
+
